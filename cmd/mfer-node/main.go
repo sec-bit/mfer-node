c6adad939fb6b00c4e6d5931feb9e0d73c9f4cb3
Steven <dynm@outlook.com>
2022-08-17 14:41:09 +0800

    upgrade version to 0.1.3

--- cmd/mfer-node/main.go
+++ cmd/mfer-node/main.go
@@ -36,7 +36,7 @@ func defaultKeyCacheFilePath() string {
 	return path.Join(cacheDir, fileName)
 }
 
-const VERSION = "0.1.2"
+const VERSION = "0.1.3"
 
 func main() {
 	account := flag.String("account", "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "impersonate account")
